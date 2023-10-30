2fa18fe6ff477b9b7737d80e3a77b62aabe27303
Luiz Guilherme Reis <luiz.guilherme.dev@outlook.com>
2023-10-30 10:14:20 -0300

    data interpolation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './componets/first-component/first-component.component';
+import { SecondComponentComponent } from './componets/first-component/second-component/second-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponentComponent
+    FirstComponentComponent,
+    SecondComponentComponent
   ],
   imports: [
     BrowserModule
